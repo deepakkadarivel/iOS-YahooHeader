a74b755c2dac8664d3eb0730193db76f3080fcbc
Deepak <deepak.kadarivel@gmail.com>
2016-01-31 01:09:51 +0530

    Header view

---
+++ YahooHeader/HeaderView.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderView.swift
+//  YahooHeader
+//
+//  Created by Deepak Kadarivel on 30/01/16.
+//  Copyright © 2016 upbeatios. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    @IBOutlet var imageView: UIImageView!
+
+}
--- YahooHeader/ViewController.swift
+++ YahooHeader/ViewController.swift
@@ -9,17 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    private let tableHeaderHeight: CGFloat = 350.0
+    private let tableHeaderCutAway: CGFloat = 50.0
+    
+    private var headerView: HeaderView!
+    private var headerMaskLayer: CAShapeLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        headerView = tableView.tableHeaderView as! HeaderView
+        tableView.tableHeaderView = nil
+        tableView.addSubview(headerView)
+        
+        tableView.contentInset = UIEdgeInsets(top: tableHeaderHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentOffset = CGPoint(x: 0, y: -tableHeaderHeight)
+        
+        headerMaskLayer = CAShapeLayer()
+        headerMaskLayer.fillColor = UIColor.blackColor().CGColor
+        headerView.layer.mask = headerMaskLayer
+        
+        updateHeaderView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        updateHeaderView()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateHeaderView()
+    }
+    
+    func updateHeaderView() {
+        let effectiveHeight = tableHeaderHeight - tableHeaderCutAway / 2
+        var headerRect = CGRect(x: 0, y: -effectiveHeight, width: tableView.bounds.width, height: tableHeaderHeight)
+        
+        if tableView.contentOffset.y < effectiveHeight {
+            headerRect.origin.y = tableView.contentOffset.y
+            headerRect.size.height = -tableView.contentOffset.y + tableHeaderCutAway / 2
+            
+        }
+        
+        headerView.frame = headerRect
+        
+        let path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: 0, y: 0))
+        path.addLineToPoint(CGPoint(x: headerRect.width, y: 0))
+        path.addLineToPoint(CGPoint(x: headerRect.width, y: headerRect.height))
+        path.addLineToPoint(CGPoint(x: 0, y: headerRect.height - tableHeaderCutAway))
+        headerMaskLayer.path = path.CGPath
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        return cell
+    }
 }
 
